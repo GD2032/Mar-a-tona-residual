cc4a2b33e96dbbffb1169f9ee1b9f9e670ca05fd
GD2032 <gabrieldias2032@gmail.com>
2019-03-17 23:18:31 -0300

    Versão 0.1

---
+++ Assets/Scripts/comportamentoCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comportamentoCamera : MonoBehaviour
+{
+    public float speed;
+    public GameObject personagem;
+    float posicaoPersonagem;
+    float variavel;
+    void Start()
+    {
+        posicaoPersonagem = personagem.transform.position.y;
+    }
+    /// <summary>
+    /// A camera acompanha o movimento do personagem no eixo Y
+    /// </summary>
+    void Update()
+    {
+        variavel = Input.GetAxis("Vertical");
+      if (personagem.transform.position.y != posicaoPersonagem)
+        {
+            transform.Translate(Vector3.up * variavel * speed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/playerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehaviour : MonoBehaviour
+{
+    public float speed;
+    public static bool movimento;
+
+    public float xmax;
+    public float ymax;
+
+    public float movimentoEixoX;
+    public float movimentoEixoY;
+
+    void Start()
+    {
+        movimento = true;
+    }
+
+    void Update()
+    {
+        movimentacao();
+    }
+
+    void movimentacao()
+    {
+        movimentoEixoX = Input.GetAxis("Horizontal");
+        movimentoEixoY = Input.GetAxis("Vertical");
+        if (movimento)
+        {
+            transform.Translate(Vector3.right * movimentoEixoX * speed * Time.deltaTime);
+            transform.Translate(Vector3.up * movimentoEixoY * speed * Time.deltaTime);
+        }
+    }
+}
